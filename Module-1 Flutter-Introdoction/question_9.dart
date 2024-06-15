857e91448570b7e9be38fcf68cd735442bd8cb37
Manish20r <manirathore637527@gmail.com>
2024-06-15 15:44:05 +0530

    Update question_9.dart

--- Module-1 Flutter-Introdoction/question_9.dart
+++ Module-1 Flutter-Introdoction/question_9.dart
@@ -1,1 +1,16 @@
 
+import 'dart:io';
+
+void main(){
+  int num1,num2;
+
+  print("enter number1 :");
+  num1=int.parse(stdin.readLineSync()!);
+  print("enter number2 :");
+  num2=int.parse(stdin.readLineSync()!);
+  print("before number1:$num1 and number2:$num2");
+  num1=num1+num2;
+  num2=num1-num2;
+  num1=num1-num2;
+  print("after number1:$num1 and number2:$num2");
+}
